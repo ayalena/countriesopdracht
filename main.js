b05147c7efe0eea8175ba5728308bcd1f062e7cf
ayalena <82506563+ayalena@users.noreply.github.com>
2021-09-08 23:00:50 +0200

    second commit

--- main.js
+++ main.js
@@ -1,56 +1,87 @@
-//1. Maak een 'Zoek'-knop op de pagina en koppel deze aan een functie die de gegevens over België ophaalt en dit in de console logt.
+//make a search button that fetches the country data when you click it
 const searchButton = document.getElementById("search-button");
 
-// let countryInput = "";
+searchButton.addEventListener('click', (event) => {
+    fetchData();
+});
 
-async function fetchData() {
-    try {
+//make a search bar that fetches the country data when pressing enter
+const searchBar = document.getElementById('input-field');
+searchBar.addEventListener('keyup', setQuery)
 
-        // this.name = countryInput;
-        // countryInput = {name};
-        const result = await axios.get("https://restcountries.eu/rest/v2/name/Belgium");
-        // getInput(name);
-        // const result = await axios.get("https://restcountries.eu/rest/v2/name/{name}");
-        const countryInfo = result.data[0];
+let query = '';
 
-        //log country data
-        console.log(countryInfo);
-
-        //2. log country name, region and population
-        console.log(`${countryInfo.name} is situated in ${countryInfo.subregion}. It has a population of ${countryInfo.population} people.`);
-        //3. log country capital
-        console.log(`The capital is ${countryInfo.capital}`);
-
-        //7. Zorg ervoor dat de opgehaalde data op de volgende manier wordt toegevoegd aan de DOM
-        const countryname = document.createElement('p');
-        countryname.textContent = `${countryInfo.name}`;
-        namecountry.appendChild(countryname);
-
-        const countrypop = document.createElement('p');
-        countrypop.textContent = `${countryInfo.name} is situated in ${countryInfo.subregion}. It has a population of ${countryInfo.population} people.`;
-        countrypops.appendChild(countrypop);
-
-        const capital = document.createElement(`p`);
-        capital.textContent = `The capital is ${countryInfo.capital} and you can pay with ${getCurrency(countryInfo.currencies)}'s.`;
-        capitalcurrency.appendChild(capital);
-
-        const languages = document.createElement('p');
-        languages.textContent = `They speak ${getLanguages(countryInfo.languages)}`;
-        countrylanguage.appendChild(languages);
-
-    } catch(e) {
-        console.error(e);
+function setQuery(e) {
+    query = e.target.value;
+    if (e.keyCode === 13) {
+        fetchData();
     }
 }
 
-searchButton.addEventListener('click', (event) => {
-    getInput(event);
-    fetchData(event);
-});
+//make a container for all the info about the country
+const countryContainer = document.getElementById("countryInformation")
 
-//4. Maak een functie die ongeacht het aantal currencies die in een land gebruikt worden
-function getCurrency (currencies) {
-    //log 1 or more currencies
+//asynchronous function for getting data about the country
+async function fetchData() {
+    searchBar.value = "";
+    //make sure the previous result gets cleared
+    // ????
+    const previousResult = document.getElementById("countryAll");
+    if (previousResult) {
+        countryContainer.removeChild(previousResult);
+    };
+    try {
+        //GET request
+        const result = await axios.get(`https://restcountries.eu/rest/v2/name/${query}`);
+        console.log(query);
+        const country = result.data[0];
+
+        //log country data, name, region, population and capital in the console
+        console.log(country);
+        console.log(`${country.name} is situated in ${country.subregion}. It has a population of ${country.population} people.`);
+        console.log(`The capital is ${country.capital}`);
+
+        //countryAll will hold all the requested data about the countries
+        const countryAll = document.createElement("div");
+        countryAll.setAttribute("div", "countryInformation");
+
+        //put the requested country data in the DOM
+        const countryName = document.createElement("h1");
+        countryName.textContent = `${country.name}`;
+        countryInformation.appendChild(countryName);
+
+        const countryRegionPopulation = document.createElement("p");
+        countryRegionPopulation.textContent = `${country.name} is situated in ${country.subregion}. It has a population of ${country.population} people.`;
+        countryInformation.appendChild(countryRegionPopulation);
+
+        const countryCapital = document.createElement("p");
+        countryCapital.textContent = `The capital is ${country.capital} and you can pay with ${getCurrency(country.currencies)}'s.`;
+        countryInformation.appendChild(countryCapital);
+
+        const countryLanguages = document.createElement("p");
+        countryLanguages.textContent = `They speak ${getLanguages(country.languages)}`;
+        countryInformation.appendChild(countryLanguages);
+
+        //put all the information about the countries in the container
+        countryContainer.appendChild(countryAll);
+
+        //get the ${country.flag} as an image in the DOM
+        //???
+        // const countryFlag = document.createElement("img");
+        // countryFlag.setAttribute("img", `${country.flags}`);
+        // countryInformation.appendChild(countryFlag);
+
+    } catch (e) {
+        console.error(e);
+        //display the error message in the DOM
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = `This country does not exist`;
+        countryInformation.appendChild(errorMessage);
+    }
+}
+
+//a function to get the countries currencies
+function getCurrency(currencies) {
     if (currencies.length === 1) {
         return currencies[0].name;
     } else if (currencies.length === 2) {
@@ -58,9 +89,8 @@ function getCurrency (currencies) {
     }
 }
 
-//6. Maak een functie die ongeacht het aantal talen die in een land gesproken worden
+//a function to get the countries' spoken languages
 function getLanguages(languages) {
-    //log one or more languages
     if (languages.length === 1) {
         return languages[0].name;
     } else if (languages.length === 2) {
@@ -68,26 +98,26 @@ function getLanguages(languages) {
     } else if (languages.length === 3) {
         return `${languages[0].name}, ${languages[1].name} and ${languages[2].name}`;
     }
+
 }
 
-//8. Maak een inputveld op de pagina en zorg ervoor dat als de gebruiker op enter drukt, de functie wordt aangeroepen waarmee de gegevens over België worden opgehaald.
+// let countryInput = "";
+// const field = document.getElementById("input-field");
 
-const field = document.getElementById("input-field");
+// function getInput (event) {
+//     const countryInput = (event.target.name, event.target.value);
+//     // console.log(countryInput);
+//     return countryInput;
+// };
 
-function getInput (event) {
-    const countryInput = (event.target.name, event.target.value);
-    console.log(countryInput);
-    return countryInput;
-};
-
-field.addEventListener("keyup", function(event) {
-    if (event.code === 'Enter') {
-        getInput(event);
-        fetchData(event);
-    }
-});
-
-//9 Zorg ervoor dat de waarde uit het input veld wordt gebruikt als query voor het GET request. Er moet alleen een request gedaan worden als de gebruiker op enter drukt, of op de zoek-knop klikt. Tip: gebruik een globale variabele.
-
-
-
+// field.addEventListener("keyup", function(event) {
+//     if (event.code === 'Enter') {
+//         getInput(event);
+//         fetchData();
+//         //10. Zorg ervoor dat de waarde van het input veld wordt leeggemaakt na elke zoekopdracht.
+//         function clearThis(field) {
+//             field.value= "";
+//         }
+//         clearThis(field);
+//     }
+// });
